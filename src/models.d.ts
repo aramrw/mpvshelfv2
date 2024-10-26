ea21633f94ace92a01a577081a011512f40e6b09
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-25 23:16:25 -0700

    Create models.d.ts

---
+++ src/models.d.ts
@@ -0,0 +1,14 @@
+export type UserType = {
+  id: string,
+  username: string,
+}
+
+export type UserFormType = {
+  username: string,
+}
+
+export type OsFolder = {
+  user_id: string,
+  path: string,
+  cover_img: string,
+}
