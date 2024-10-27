a5cfb9bd3ffea64878187313b5061b22bff21b22
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-26 23:46:00 -0700

    Update add-new-skeleton.tsx

--- src/dashboard/components/add-new-skeleton.tsx
+++ src/dashboard/components/add-new-skeleton.tsx
@@ -1,43 +1,68 @@
 import { IconFilePlus, IconFolderPlus, IconPlus } from "@tabler/icons-solidjs";
 import { Popover, PopoverContent, PopoverTrigger } from "../../components/ui/popover";
-import readOsFolderDir from "../../tauri-cmds/read_os_folder_dir";
+import { read_os_folder_dir } from "../../tauri-cmds/os_folders";
 import { open } from '@tauri-apps/plugin-dialog';
-import { UserType } from "src/models";
+import { OsFolder, UserType } from "src/models";
+import { Accessor, Setter } from "solid-js";
+import { Transition } from "solid-transition-group";
 
-const AddNewSkeleton = ({ user }: { user: UserType | null }) => {
+const AddNewSkeleton = (
+  { user, setOsFolders }:
+    {
+      user: Accessor<UserType | null>,
+      setOsFolders: Setter<OsFolder[]>
+    }
+) => {
   return (
-    <Popover placement="right-start">
-      <PopoverTrigger>
-        <div class="w-fit relative flex items-center hover:opacity-50 transition-opacity cursor-pointer duration-100 ease-in-out">
-          <div class="h-32 w-24 rounded-sm shadow-md bg-white" />
-          <IconPlus class="w-9 h-9 absolute text-zinc-300 fill-red-500 stroke-[1.8] left-[30px] top-15" />
-        </div>
-      </PopoverTrigger>
-      <PopoverContent class="p-0 bg-transparent border-none shadow-none text-sm flex flex-col justify-center items-center font-medium">
-        <ul class="w-full h-full flex flex-col gap-1">
-          <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
-            on:click={() => {
-              console.log("opening finder")
-              if (user) {
-                open({ directory: true }).then((dir_path) => {
-                  if (dir_path) {
-                    readOsFolderDir(dir_path, user.id).then((osFolder: unknown) => {
-                      console.log(osFolder);
-                    });
-                  }
-                });
-              }
-            }}
-          >
-            <IconFolderPlus class="w-4 h-4 text-primary fill-zinc-500 stroke-[2.5]" />
-          </li>
-          <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
-          >
-            <IconFilePlus class="w-4 h-4 text-primary fill-zinc-500 stroke-[2.5]" />
-          </li>
-        </ul>
-      </PopoverContent>
-    </Popover>
+    <Transition
+      appear={true}
+      onEnter={(el, done) => {
+        const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+          duration: 600
+        });
+        a.finished.then(done);
+      }}
+      onExit={(el, done) => {
+        const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+          duration: 600
+        });
+        a.finished.then(done);
+      }}
+    >
+
+      <Popover placement="right-start">
+        <PopoverTrigger class="h-fit">
+          <div class="w-fit relative flex items-center justify-center hover:opacity-50 transition-opacity cursor-pointer duration-100 ease-in-out">
+  <div class="h-32 w-24 sm:h-44 sm:w-32 md:h-48 md:w-36 lg:h-52 lg:w-40 rounded-sm shadow-md bg-white" />
+  <IconPlus class="w-9 h-9 sm:h-11 sm:w-11 md:h-14 md:w-14 lg:h-16 lg:w-16 absolute text-zinc-300 fill-red-500 stroke-[1.8] top-0 bottom-0 left-0 right-0 m-auto" />
+</div>
+        </PopoverTrigger>
+        <PopoverContent class="p-0 bg-transparent border-none shadow-none text-sm flex flex-col justify-center items-center font-medium">
+          <ul class="w-fit h-full flex flex-col gap-1">
+            <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
+              onClick={() => {
+                if (user()) {
+                  open({ directory: true }).then((dir_path) => {
+                    if (dir_path) {
+                      read_os_folder_dir(dir_path, user()!.id).then((newOsFolder: unknown) => {
+                        console.log(newOsFolder);
+                        setOsFolders((prev) => [...(prev ?? []), newOsFolder as OsFolder]);
+                      });
+                    }
+                  });
+                }
+              }}
+            >
+              <IconFolderPlus class="w-6 h-6 sm:h-8 sm:w-8 md:h-9 md:w-9 lg:w-10 lg:h-10 text-primary fill-zinc-500 stroke-[2.2]" />
+            </li>
+            <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
+            >
+              <IconFilePlus class="w-6 h-6 sm:h-8 sm:w-8 md:h-9 md:w-9 lg:h-10 lg:w-10 text-primary fill-zinc-500 stroke-[2.2]" />
+            </li>
+          </ul>
+        </PopoverContent>
+      </Popover>
+    </Transition>
   );
 };
 
