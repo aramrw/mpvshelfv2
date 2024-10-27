ded1659d4484e090ec9e63a2392f9b6de559dc12
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-26 23:45:46 -0700

    Update database.rs

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,10 +1,10 @@
 use std::{path::PathBuf, sync::LazyLock};
 
-use data::v1::{User, UserKey};
+use data::v1::{OsFolder, OsFolderKey, User};
 use native_db::*;
 use tauri::{command, AppHandle, Manager};
 
-use crate::error::DatabaseError;
+use crate::{error::DatabaseError, misc::get_date_time};
 
 pub mod data {
     use native_db::{native_db, ToKey};
@@ -12,8 +12,6 @@ pub mod data {
     use serde::{Deserialize, Serialize};
 
     pub mod v1 {
-        use std::ffi::OsString;
-
         use super::*;
 
         #[derive(Serialize, Deserialize, Debug)]
@@ -26,24 +24,33 @@ pub mod data {
             pub username: String,
         }
 
-        #[derive(Serialize, Deserialize, Debug)]
-        #[native_model(id = 2, version = 1)]
-        #[native_db]
-        pub struct GlobalUser {
-            #[primary_key]
-            pub id: String,
-            pub username: String,
-        }
-
-        #[derive(Serialize, Deserialize, Debug)]
+        #[derive(Serialize, Deserialize, Clone, Debug)]
         #[native_model(id = 3, version = 1)]
         #[native_db]
         pub struct OsFolder {
-            #[primary_key]
-            pub user_id: String,
             #[secondary_key]
+            pub user_id: String,
+            #[primary_key]
             pub path: String,
+            pub title: String,
+            pub os_videos: Vec<OsVideo>,
             pub cover_img_path: Option<String>,
+            pub update_date: String,
+            pub update_time: String,
+        }
+
+        #[derive(Serialize, Deserialize, Clone, Debug)]
+        #[native_model(id = 4, version = 1)]
+        #[native_db]
+        pub struct OsVideo {
+            #[secondary_key]
+            pub user_id: String,
+            #[primary_key]
+            pub path: String,
+            pub title: String,
+            pub cover_img_path: Option<String>,
+            pub update_date: String,
+            pub update_time: String,
         }
     }
 }
@@ -51,6 +58,8 @@ pub mod data {
 static DBMODELS: LazyLock<Models> = LazyLock::new(|| {
     let mut models = Models::new();
     models.define::<data::v1::User>().unwrap();
+    models.define::<data::v1::OsFolder>().unwrap();
+    models.define::<data::v1::OsVideo>().unwrap();
     models
 });
 
@@ -63,6 +72,68 @@ pub fn init_database(app_data_dir: &PathBuf, handle: &AppHandle) -> Result<(), d
     Builder::new().create(&DBMODELS, &db_path)?;
 
     handle.manage(db_path);
+    Ok(())
+}
+
+#[command]
+pub fn get_os_folders(handle: AppHandle, user_id: String) -> Result<Vec<OsFolder>, DatabaseError> {
+    let db_path = handle.state::<PathBuf>().to_string_lossy().to_string();
+    let db = Builder::new().create(&DBMODELS, db_path)?;
+
+    let rtx = db.r_transaction()?;
+    let folders: Vec<OsFolder> = rtx
+        .scan()
+        .secondary(OsFolderKey::user_id)?
+        .start_with(user_id.as_str())?
+        .try_collect()?;
+
+    if folders.is_empty() {
+        return Err(DatabaseError::OsFoldersNotFound(format!(
+            "0 OsFolders found belonging to user_id: {user_id}",
+        )));
+    }
+
+    Ok(folders)
+}
+
+#[command]
+pub fn update_os_folders(
+    handle: AppHandle,
+    os_folders: Vec<OsFolder>,
+) -> Result<(), DatabaseError> {
+    let db_path = handle.state::<PathBuf>().to_string_lossy().to_string();
+    let db = Builder::new().open(&DBMODELS, db_path)?;
+    let rtx = db.rw_transaction()?;
+    let (date, time) = get_date_time();
+
+    for mut folder in os_folders {
+        folder.update_date = date.clone();
+        folder.update_time = time.clone();
+
+        rtx.upsert(folder)?;
+    }
+
+    rtx.commit()?;
+
+    Ok(())
+}
+
+#[command]
+pub fn delete_os_folders(
+    handle: AppHandle,
+    os_folders: Vec<OsFolder>,
+) -> Result<(), DatabaseError> {
+    let db_path = handle.state::<PathBuf>().to_string_lossy().to_string();
+    let db = Builder::new().open(&DBMODELS, db_path)?;
+
+    let rtx = db.rw_transaction()?;
+
+    for folder in os_folders {
+        rtx.remove(folder)?;
+    }
+
+    rtx.commit()?;
+
     Ok(())
 }
 
