8c2b40a206c3e73e4c32f94e7621ffcf430dcd4d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-26 23:46:06 -0700

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,27 +1,50 @@
-import { createSignal, onMount, Suspense } from "solid-js";
+import { createSignal, For, onMount } from "solid-js";
 import NavBar from "../main-components/navbar"
-import { UserType } from "src/models";
-import getDefaultUser from "../tauri-cmds/get_default_user";
+import { OsFolder, UserType } from "src/models";
 import AddNewSkeleton from "./components/add-new-skeleton";
+import OsFolderCard from "./components/os-folder-card";
+import { get_os_folders } from "../tauri-cmds/os_folders";
+import { get_default_user } from "../tauri-cmds/users";
+
+function SkeletonPlaceholder({ count }: { count: number }) {
+  return Array(count)
+    .fill(null)
+    .map((_, index) => (
+      <div
+        class="bg-gray-200 animate-pulse rounded-md h-[calc(cardHeight)] w-full"
+      />
+    ));
+}
 
 export default function Dashboard() {
   const [user, setUser] = createSignal<UserType | null>(null);
-  const [folders, setFolders] = createSignal();
+  const [osFolders, setOsFolders] = createSignal<OsFolder[]>([]);
 
   onMount(() => {
-    getDefaultUser().then((user: unknown) => {
+    get_default_user().then((user: unknown) => {
       setUser(user as UserType);
-      console.log(user);
-    })
+    }).then(() => {
+      if (user()) {
+        get_os_folders(user()!.id).then((folders: unknown) => {
+          setOsFolders(folders as OsFolder[]);
+          console.log(folders);
+        });
+      }
+    });
   });
 
   return (
     <main>
       <NavBar />
-      <section class="p-3">
-        {user && (
-          <AddNewSkeleton user={user()} />
-        )}
+      <section class="h-fit p-3 md:px-16 lg:px-36 xl:px-44 flex flex-row gap-2">
+        <div class="grid grid-cols-4 gap-2 min-h-[calc(4*cardHeight)]">
+          <For each={osFolders()} fallback={<SkeletonPlaceholder count={4} />}>
+            {(folder) => (
+              <OsFolderCard folder={folder} user={user} setOsFolders={setOsFolders} />
+            )}
+          </For>
+          <AddNewSkeleton user={user} setOsFolders={setOsFolders} />
+        </div>
       </section>
     </main>
   );
