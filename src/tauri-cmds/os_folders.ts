4a30842fe48a6910ebcfd9cef6d6c56320a26765
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-26 23:46:20 -0700

    Create os_folders.ts

---
+++ src/tauri-cmds/os_folders.ts
@@ -0,0 +1,46 @@
+import { invoke } from "@tauri-apps/api/core";
+import { OsFolder } from "src/models";
+
+const get_os_folders = async (userId: string) => {
+  try {
+    const osFolders: OsFolder[] = await invoke("get_os_folders", { userId });
+    return osFolders;
+  } catch (error) {
+    console.error("get_os_folders:", error);
+    return null;
+  }
+}
+
+const update_os_folders = async (osFolders: OsFolder[], userId: string) => {
+  try {
+    const osFolder: OsFolder = await invoke("update_os_folders", { osFolders, userId });
+    return osFolder;
+  } catch (error) {
+    console.error("update_os_folders", error);
+    return null;
+  }
+}
+
+const delete_os_folders = async (osFolders: OsFolder[], userId: string) => {
+  try {
+    const osFolder: OsFolder = await invoke("delete_os_folders", { osFolders, userId });
+    return osFolder;
+  } catch (error) {
+    console.error("delete_os_folders", error);
+    return null;
+  }
+}
+
+
+const read_os_folder_dir = async (path: string, userId: string, coverImgPath?: string, updateDatetime?: string[]) => {
+  try {
+    const osFolder: OsFolder = await invoke("read_os_folder_dir", { path, userId, coverImgPath, updateDatetime });
+    return osFolder;
+  } catch (error) {
+    console.error("read_os_folder_dir", error);
+    return null;
+  }
+}
+
+
+export { read_os_folder_dir, get_os_folders, delete_os_folders, update_os_folders };
