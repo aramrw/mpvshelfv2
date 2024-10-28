ee83172648801c9e0955e23e5eb1f80c2b0da5be
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-27 17:04:43 -0700

    Update error.rs

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -15,19 +15,21 @@ pub enum DatabaseError {
     IoError(#[from] io::Error),
     #[error("{0:#?}")]
     TuariError(#[from] tauri::Error),
-
 }
 
 #[derive(thiserror::Error, Debug)]
-pub enum IoError {
-    #[error("{0:#?}")]
-    NativeDbError(#[from] Error),
-    #[error("{0:#?}")]
-    IoError(#[from] io::Error),
+pub enum MpvError {
+    #[error("MPV Player was not found on the System PATH.")]
+    SudoPATHNotFound,
+    #[error("MPV Player was not found @ the specified path: {0}")]
+    AbsolutePathNotFound(String),
+    #[error("Failed to execute MPV Player: {0}")]
+    CmdError(#[from] io::Error),
+
 }
 
-impl From<IoError> for InvokeError {
-    fn from(error: IoError) -> Self {
+impl From<MpvError> for InvokeError {
+    fn from(error: MpvError) -> Self {
         InvokeError::from_error(error)
     }
 }
