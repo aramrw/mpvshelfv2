4df26dd7429d6863b7e4279d52ff3599b4100a2c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-27 17:04:59 -0700

    Update models.d.ts

--- src/models.d.ts
+++ src/models.d.ts
@@ -1,6 +1,14 @@
 export type UserType = {
   id: string,
   username: string,
+  settings: SettingsType,
+}
+
+export type SettingsType = {
+  user_id: string,
+  mpv_path: string,
+  update_date: string,
+  update_time: string,
 }
 
 export type UserFormType = {
@@ -10,18 +18,18 @@ export type UserFormType = {
 export type OsFolder = {
   user_id: string,
   path: string,
-	title: string,
-	os_videos: OsVideo[], 
+  title: string,
+  os_videos: OsVideo[],
   cover_img_path: string | undefined,
-	update_date: string,
-	update_time: string,
+  update_date: string,
+  update_time: string,
 }
 
 export type OsVideo = {
   user_id: string,
   path: string,
-	title: string,
+  title: string,
   cover_img_path: string | undefined,
-	update_date: string,
-	update_time: string,
+  update_date: string,
+  update_time: string,
 }
