7c47efc904fabd6c31e10aff1e57d32691457d9d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-27 17:04:47 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import { onMount } from "solid-js";
 import "./App.css";
-import Navbar from "./main-components/navbar";
-//import { User } from "../@/libs/models";
 import { useNavigate } from "@solidjs/router";
-import { get_default_user } from "./tauri-cmds/users";
+import get_default_user from "./tauri-cmds/users";
 
 function App() {
   const navigate = useNavigate();
 
   onMount(async () => {
-    get_default_user().then((user: unknown) => {
-      if (user) {
-        navigate("/dashboard");
-      } else {
-        navigate("/create-profile")
-      }
-    })
+    let defaultUser = await get_default_user();
+    if (defaultUser) {
+      navigate("/dashboard");
+    } else {
+      navigate("/create-profile")
+    }
   });
 
   return (
     <main>
+			
     </main>
   );
 }
