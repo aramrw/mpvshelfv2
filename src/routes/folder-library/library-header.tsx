449f25b9c78236895e726021588ec93ff53af668
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:40 -0700

    Update library-header.tsx

--- src/routes/folder-library/library-header.tsx
+++ src/routes/folder-library/library-header.tsx
@@ -1,36 +1,27 @@
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { Show } from "solid-js";
-import { OsFolder } from "src/models";
+import { OsFolder, UserType } from "../../models";
+import play_video from "../../tauri-cmds/mpv/play_video";
 
-export default function ({ mainParentFolder }: { mainParentFolder: OsFolder }) {
+export default function ({ mainParentFolder, user }: { mainParentFolder: OsFolder, user: UserType }) {
+
   return (
-    <header class="w-full h-64 md:h-72 lg:h-[330px] py-3 px-2 relative border-b-white border-b-2">
-      <div
-        class="absolute inset-0 z-0"
-        style={{
-          "background-image": `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.2)),url(${convertFileSrc(mainParentFolder?.cover_img_path!)})`,
-          "background-size": "cover",
-          "background-repeat": "no-repeat",
-          "background-position": "center",
-          filter: "blur(6px)",
-        }}
-      />
-      <svg
-        class="text-zinc-700 fill-zinc-700 hover:text-zinc-400 hover:fill-zinc-400 transition-all
-           cursor-pointer h-12 w-12 p-1
-					mix-blend-plus-lighter absolute 
-					left-[5px] top-[75px] z-50"
-        xmlns="http://www.w3.org/2000/svg"
-        width="30"
-        height="30"
-        viewBox="0 0 24 24"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round">
-        <polygon points="6 3 20 12 6 21 6 3" />
-      </svg>
+    <header class="sm:px-2 md:px-16 lg:px-30 xl:px-40  w-full h-64 md:h-72 lg:h-[330px] py-3 px-2 relative">
+      <Show when={mainParentFolder}>
+        <div
+          class="absolute inset-0 z-0"
+          style={{
+            "background-image":
+              `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.2)),url(${convertFileSrc(mainParentFolder?.last_watched_video.cover_img_path!)})`,
+            "background-size": "cover",
+            "background-repeat": "no-repeat",
+            "background-position": "center",
+            filter: "blur(6px)",
+          }}
+        />
+      </Show>
       <h1 class="text-zinc-600 bg-transparent mix-blend-difference w-fit font-semibold z-10 relative text-medium md:text-xl 
-				lg:text-2xl shadow-2xl rounded-none px-0.5 border-zinc-600 border-2 mb-1" >
+        lg:text-2xl shadow-2xl rounded-none px-0.5 border-zinc-600 border-2 mb-1">
         {mainParentFolder?.title}
       </h1>
       <div class="w-fit flex flex-row items-center gap-1">
@@ -41,17 +32,38 @@ export default function ({ mainParentFolder }: { mainParentFolder: OsFolder }) {
           {mainParentFolder?.update_time}
         </h3>
       </div>
-      <div class="flex flex-row justify-start items-start gap-2">
+      <div class="relative w-fit flex items-start gap-2"> {/* Flex container for the image and SVG */}
         <Show when={mainParentFolder.title && mainParentFolder.cover_img_path}>
           <img
             alt={mainParentFolder?.title}
-            src={convertFileSrc(mainParentFolder?.cover_img_path!)}
-            class="select-none relative h-[175px] md:h-[200px] object-contain lg:h-[240px] 
-						w-auto z-100 rounded-none 
-						border-transparent border-2 shadow-md"
+            src={convertFileSrc(mainParentFolder?.last_watched_video.cover_img_path!)}
+            class="select-none h-[175px] md:h-[200px] object-contain lg:h-[240px] 
+              w-auto z-30 rounded-none 
+              border-transparent border-2 shadow-md"
           />
+          <svg
+            class="text-secondary fill-accent hover:text-muted bg-primary/50 rounded-sm 
+						hover:fill-secondary/60 hover:bg-secondary/20 transition-all
+              cursor-pointer h-auto w-1/4 p-1 mix-blend-plus-lighter absolute z-50 shadow-md shadow-primary/20"
+            style={{
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)' // Center the Play button within the image
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+						onClick={() => play_video(mainParentFolder, mainParentFolder.last_watched_video, user)}
+          >
+            <polygon points="6 3 20 12 6 21 6 3" />
+          </svg>
         </Show>
       </div>
     </header>
-  )
+  );
 }
+
