bd12b4ff950a691c39472c1f883013f15a289985
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:27 -0700

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,24 +1,14 @@
 import { createSignal, For, onMount, Show } from "solid-js";
 import NavBar from "../main-components/navbar"
-import { OsFolder, UserType } from "src/models";
+import { OsFolder, UserType } from "../models";
 import AddNewSkeleton from "./components/add-new-skeleton";
 import OsFolderCard from "./components/os-folder-card";
 import { get_os_folders } from "../tauri-cmds/os_folders";
 import get_default_user from "../tauri-cmds/users";
 import mpv_system_check from "../tauri-cmds/mpv/mpv_system_check";
-import { ToastRegion, ToastList } from "../components/ui/toast";
 import ErrorAlert from "../main-components/error-alert";
 import { useNavigate } from "@solidjs/router";
-
-function SkeletonPlaceholder({ count }: { count: number }) {
-  return Array(count)
-    .fill(null)
-    .map((_, _index) => (
-      <div
-        class="bg-gray-200 animate-pulse rounded-md h-[calc(cardHeight)] w-full"
-      />
-    ));
-}
+import { Skeleton } from "../components/ui/skeleton";
 
 export default function Dashboard() {
   const [user, setUser] = createSignal<UserType | null>(null);
@@ -30,10 +20,9 @@ export default function Dashboard() {
     let defaultUser = await get_default_user();
     if (defaultUser) {
       setUser(defaultUser);
-      get_os_folders(defaultUser.id).then((folders: unknown) => {
-        setOsFolders(folders as OsFolder[]);
-        console.log(folders);
-      });
+      const folders = await get_os_folders(defaultUser.id);
+      setOsFolders(folders as OsFolder[]);
+      console.log(folders);
     }
     let error = await mpv_system_check();
     setError(error);
@@ -43,15 +32,19 @@ export default function Dashboard() {
     <main>
       <NavBar />
       <Show when={error()}>
-        <ErrorAlert error={error()!} onClick={() => navigate("/settings/mpv?ERROR")} />
+        <ErrorAlert error={error()!} onClick={() => navigate(`/settings/mpv_ERROR_${error()}`)} />
       </Show>
-      <section class="h-fit p-3 md:px-16 lg:px-36 xl:px-44 flex flex-row gap-2">
+      <section class="h-fit py-4 px-3 md:px-16 lg:px-36 xl:px-44 flex flex-row gap-2">
         <div class="grid grid-cols-4 gap-2 min-h-[calc(4*cardHeight)]">
-          <For each={osFolders()} fallback={<SkeletonPlaceholder count={4} />}>
-            {(folder) => (
-              <OsFolderCard folder={folder} user={user} setOsFolders={setOsFolders} />
-            )}
-          </For>
+          <Show when={osFolders() && osFolders().length > 0} fallback={
+            <Skeleton class="h-32 w-24 sm:h-44 sm:w-32 md:h-48 md:w-36 lg:h-52 lg:w-40" />
+          }>
+            <For each={osFolders()}>
+              {(folder) => (
+                <OsFolderCard folder={folder} user={user} setOsFolders={setOsFolders} />
+              )}
+            </For>
+          </Show>
           <AddNewSkeleton user={user} setOsFolders={setOsFolders} />
         </div>
       </section>
