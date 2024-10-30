e8efe1324e1dafc197174775435d2da069a6b59d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:52 -0700

    Create get_user_by_id.ts

---
+++ src/tauri-cmds/get_user_by_id.ts
@@ -0,0 +1,13 @@
+import { invoke } from "@tauri-apps/api/core";
+import { UserType } from "src/models";
+
+export default async function get_user_from_id(userId: string) {
+  try {
+    const user: UserType = await invoke("get_user_by_id", { userId });
+    return user;
+  } catch (error) {
+    console.error("get_user_id", error);
+    return null;
+  }
+};
+
