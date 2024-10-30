328cd09d57a19b4463986d99d571339142c17fca
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:55 -0700

    Update mpv_system_check.ts

--- src/tauri-cmds/mpv/mpv_system_check.ts
+++ src/tauri-cmds/mpv/mpv_system_check.ts
@@ -3,7 +3,7 @@ import { invoke } from "@tauri-apps/api/core";
 export default async function mpv_system_check(mpv_path?: string): Promise<null | string> {
   try {
     await invoke("mpv_system_check", { mpv_path });
-    return "Error: This is a test Error message.";
+    return null;
   } catch (e) {
     return e as string;
   }
