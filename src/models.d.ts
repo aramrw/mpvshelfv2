44858766a60e754f053e1994a0d22cbdbe067aea
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:35 -0700

    Update models.d.ts

--- src/models.d.ts
+++ src/models.d.ts
@@ -6,7 +6,9 @@ export type UserType = {
 
 export type SettingsType = {
   user_id: string,
-  mpv_path: string,
+  mpv_path?: string,
+  plugins_path?: string,
+  autoplay: boolean,
   update_date: string,
   update_time: string,
 }
@@ -20,6 +22,7 @@ export type OsFolder = {
   path: string,
   title: string,
   os_videos: OsVideo[],
+  last_watched_video: OsVideo,
   cover_img_path: string | undefined,
   update_date: string,
   update_time: string,
@@ -27,9 +30,11 @@ export type OsFolder = {
 
 export type OsVideo = {
   user_id: string,
+  main_folder_path: string,
   path: string,
   title: string,
   cover_img_path: string | undefined,
+  watched: bool,
   update_date: string,
   update_time: string,
 }
