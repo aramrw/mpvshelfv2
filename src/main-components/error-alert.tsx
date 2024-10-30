f82bb61f2e21f7bdc5689d7fff6258c060c58982
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-29 22:59:29 -0700

    Update error-alert.tsx

--- src/main-components/error-alert.tsx
+++ src/main-components/error-alert.tsx
@@ -1,6 +1,6 @@
+import { createSignal, Show } from "solid-js";
 import {
   AlertDialog,
-  AlertDialogClose,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
@@ -9,31 +9,48 @@ import {
   AlertDialogTrigger,
   AlertDialogAction
 } from "../components/ui/alert-dialog";
+import { Button } from "../components/ui/button";
 
 interface ErrorAlertProps {
   error: string;
-  onClick: (event: MouseEvent) => void;
+  onClick?: (event: MouseEvent) => void;
 }
 
 function ErrorAlert(props: ErrorAlertProps) {
   const [title, def] = props.error.split(":");
+  const [dialogOpen, setDialogOpen] = createSignal<boolean>(true);
 
   return (
-    <AlertDialog open={true}>
+    <AlertDialog open={dialogOpen()}>
       <AlertDialogTrigger />
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>{title}</AlertDialogTitle>
-          <AlertDialogDescription>
-            {def}
+      <AlertDialogContent class="min-h-40 min-w-40 border-destructive border-2">
+        <AlertDialogHeader class="space-y-0">
+          <h1 class="mb-4 text-2xl font-semibold bg-destructive w-fit rounded-sm text-destructive-foreground p-1">Error</h1>
+          <AlertDialogTitle class="text-md w-full">
+            <code>
+              {title}
+            </code>
+          </AlertDialogTitle>
+          <AlertDialogDescription class="font-medium rounded-sm p-2 w-fit">
+            {def}.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogAction
-            onClick={props.onClick}
-          >
-            Continue
-          </AlertDialogAction>
+          <Show
+            when={props.onClick}
+            fallback={
+              <Button
+                variant="destructive"
+                class="min-w-14 bg-destructive text-destructive-foreground hover:bg-primary"
+                onClick={() => setDialogOpen(false)}>
+                Close
+              </Button>}>
+            <AlertDialogAction
+              onClick={props.onClick}
+            >
+              Continue
+            </AlertDialogAction>
+          </Show>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
