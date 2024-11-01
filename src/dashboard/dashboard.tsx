49ecaf377ebb05e6f8bcf4d1fb693ec88fdae546
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-31 19:43:40 -0700

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, onMount, Show } from "solid-js";
+import { createEffect, createResource, createSignal, For, onMount, Show } from "solid-js";
 import NavBar from "../main-components/navbar"
 import { OsFolder, UserType } from "../models";
 import AddNewSkeleton from "./components/add-new-skeleton";
@@ -17,12 +17,17 @@ export default function Dashboard() {
   const navigate = useNavigate();
 
   onMount(async () => {
-    let defaultUser = await get_default_user();
-    if (defaultUser) {
-      setUser(defaultUser);
-      const folders = await get_os_folders(defaultUser.id);
-      setOsFolders(folders as OsFolder[]);
-      console.log(folders);
+    const user = await get_default_user();
+    if (user) {
+      setUser(user);
+      get_os_folders(user.id).then((folders) => {
+        if (folders) {
+          console.log(folders)
+          setOsFolders(folders);
+        } else {
+          console.log("no folders")
+        }
+      })
     }
     let error = await mpv_system_check();
     setError(error);
@@ -36,7 +41,7 @@ export default function Dashboard() {
       </Show>
       <section class="h-fit py-4 px-3 md:px-16 lg:px-36 xl:px-44 flex flex-row gap-2">
         <div class="grid grid-cols-4 gap-2 min-h-[calc(4*cardHeight)]">
-          <Show when={osFolders() && osFolders().length > 0} fallback={
+          <Show when={user() && osFolders() && osFolders()?.length > 0} fallback={
             <Skeleton class="h-32 w-24 sm:h-44 sm:w-32 md:h-48 md:w-36 lg:h-52 lg:w-40" />
           }>
             <For each={osFolders()}>
@@ -45,7 +50,9 @@ export default function Dashboard() {
               )}
             </For>
           </Show>
-          <AddNewSkeleton user={user} setOsFolders={setOsFolders} />
+          <Show when={user()}>
+            <AddNewSkeleton user={user} setOsFolders={setOsFolders} />
+          </Show>
         </div>
       </section>
     </main>
