825b8275655989ca911f1f7a62c139787644d509
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-31 19:44:02 -0700

    Update library.tsx

--- src/routes/folder-library/library.tsx
+++ src/routes/folder-library/library.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "@solidjs/router"
 import { OsFolder, OsVideo, UserType } from "../../models";
 import { Transition } from "solid-transition-group";
-import LibraryHeader from "./library-header";
-import LibraryVideosSection from "./library-videos-section";
+import LibraryHeader from "./header";
+import LibraryVideosSection from "./videos-section";
 import { createResource, Show } from "solid-js";
 import NavBar from "../../main-components/navbar";
 import get_user_by_id from "../../tauri-cmds/get_user_by_id";
@@ -10,34 +10,26 @@ import { get_os_videos } from "../../tauri-cmds/os_videos";
 import get_os_folder_by_path from "../../tauri-cmds/mpv/get_os_folder_by_path";
 import { Tabs, TabsContent, TabsIndicator, TabsList, TabsTrigger } from "../../components/ui/tabs";
 import { IconDeviceTvFilled, IconFolderFilled } from "@tabler/icons-solidjs";
-
-const fetchFolder = async (folderPath: string): Promise<OsFolder | null> => {
-  return await get_os_folder_by_path(folderPath);
-};
-
-const fetchUser = async (userId: string): Promise<UserType | null> => {
-  return await get_user_by_id(userId);
-}
-
-const fetchVideos = async (folderPath: string): Promise<OsVideo[] | null> => {
-  return await get_os_videos(folderPath);
-}
+import LibraryFoldersSection from "./folders-section";
+import get_os_folders_by_path from "../../tauri-cmds/get_os_folders_by_path";
 
 export default function Library() {
   const params = useParams();
   const folderPath = decodeURIComponent(params.folder).replace(/\)$/, ""); // Remove trailing )
 
-  const [mainParentFolder] = createResource(folderPath, fetchFolder);
+  const [mainParentFolder] = createResource(folderPath, get_os_folder_by_path);
 
   const [user] = createResource(
     () => mainParentFolder() ? mainParentFolder()?.user_id : null,
-    fetchUser
+    get_user_by_id
   );
 
   const [osVideos] = createResource(
     () => mainParentFolder() ? mainParentFolder()?.path : null,
-    fetchVideos
+    get_os_videos
   );
+
+  const [childFolders] = createResource(() => mainParentFolder() ? mainParentFolder()?.path : null, get_os_folders_by_path)
 
   return (
     <Transition
@@ -57,26 +49,49 @@ export default function Library() {
           defaultValue="videos"
           class="w-full"
           orientation="horizontal"
-        //value={selectedTab()}
-        //onChange={setSelectedTab}
-        //disabled={errorMessage ? true : false}
         >
-          <Show when={mainParentFolder.latest && osVideos.latest && user.latest}>
-            <LibraryHeader mainParentFolder={mainParentFolder.latest!} user={user.latest!} />
+          <Show when={mainParentFolder() && user()}>
+            <LibraryHeader
+              mainParentFolder={mainParentFolder.latest!}
+              user={user.latest!} />
             <TabsList class="w-full h-9 border">
-              <TabsTrigger value="videos" class="w-fit lg:text-base flex flex-row gap-x-0.5">
-                Videos
-                <IconDeviceTvFilled class="w-3 h-auto p-0" />
-              </TabsTrigger>
-              <TabsTrigger value="folders" class="w-fit lg:text-base folders flex flex-row gap-x-0.5">
-                Folders
-                <IconFolderFilled class="ml-0.5 w-3 stroke-[2.4px]" />
-              </TabsTrigger>
+              <Show when={osVideos()}>
+                <TabsTrigger
+                  value="videos"
+                  class="w-fit lg:text-base flex flex-row gap-x-0.5">
+                  Videos
+                  <IconDeviceTvFilled
+                    class="w-3 h-auto p-0" />
+                </TabsTrigger>
+              </Show>
+              <Show when={childFolders()}>
+                <TabsTrigger
+                  value="folders"
+                  class="w-fit lg:text-base folders flex flex-row gap-x-0.5">
+                  Folders
+                  <IconFolderFilled
+                    class="ml-0.5 w-3 stroke-[2.4px]" />
+                </TabsTrigger>
+              </Show>
               <TabsIndicator />
             </TabsList>
+            <Show when={osVideos()}>
             <TabsContent value="videos">
-              <LibraryVideosSection mainParentFolder={mainParentFolder.latest!} osVideos={osVideos.latest!} user={user.latest!} />
+              <LibraryVideosSection
+                mainParentFolder={mainParentFolder()!}
+                osVideos={osVideos()!}
+                user={user()!} />
             </TabsContent>
+						</Show>
+            <Show when={childFolders()}>
+              <TabsContent value="folders">
+                <LibraryFoldersSection
+                  user={user.latest!}
+                  mainParentFolder={mainParentFolder()!}
+                  childFolders={childFolders()!}
+                />
+              </TabsContent>
+            </Show>
           </Show>
         </Tabs>
       </main>
