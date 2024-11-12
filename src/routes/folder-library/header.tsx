3ccaea0a9fc35adebf95838d93ee418ccfca69be
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-11 18:38:49 -0800

    video header updates

--- src/routes/folder-library/header.tsx
+++ src/routes/folder-library/header.tsx
@@ -3,18 +3,16 @@ import { Show } from "solid-js";
 import { OsFolder, UserType } from "../../models";
 import play_video from "../../tauri-cmds/mpv/play_video";
 
-export default function ({ mainParentFolder, user }: { mainParentFolder: OsFolder, user: UserType }) {
-
-	const FILE_SRC_LWV_COVER_IMG_PATH = convertFileSrc(mainParentFolder?.last_watched_video?.cover_img_path!);
+export default function ({ mainParentFolder, user }: { mainParentFolder: OsFolder; user: UserType }) {
+  const FILE_SRC_LWV_COVER_IMG_PATH = convertFileSrc(mainParentFolder?.last_watched_video?.cover_img_path!);
 
   return (
-    <header class="sm:px-2 md:px-16 lg:px-30 xl:px-40  w-full h-64 md:h-72 lg:h-[330px] py-3 px-2 relative">
+    <header class="sm:px-2 md:px-16 lg:px-30 xl:px-40 w-full h-fit py-3 px-2 relative">
       <Show when={mainParentFolder}>
         <div
           class="absolute inset-0 z-0"
           style={{
-            "background-image":
-              `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.2)),url(${FILE_SRC_LWV_COVER_IMG_PATH})`,
+            "background-image": `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.2)),url(${FILE_SRC_LWV_COVER_IMG_PATH})`,
             "background-size": "cover",
             "background-repeat": "no-repeat",
             "background-position": "center",
@@ -22,36 +20,45 @@ export default function ({ mainParentFolder, user }: { mainParentFolder: OsFolde
           }}
         />
       </Show>
-      <h1 class="text-secondary/70 bg-transparent mix-blend-difference w-fit font-semibold z-10 relative text-medium md:text-xl 
-        lg:text-2xl shadow-2xl rounded-none px-0.5 border-secondary/70 border-2 mb-1">
+      <h1
+        class="text-secondary/70 bg-transparent mix-blend-difference w-fit font-semibold z-10 relative text-medium md:text-xl
+        lg:text-2xl shadow-2xl rounded-none px-0.5 border-secondary/70 border-2 mb-1"
+      >
         {mainParentFolder?.title}
       </h1>
       <div class="w-fit flex flex-row items-center gap-1">
-        <h2 class="text-secondary/50 mb-2 text-xs w-fit font-semibold z-15 relative 
-					bg-transparent mix-blend-difference rounded-none border-secondary/50 border-[1.5px] px-1 shadow-md">
+        <h2
+          class="text-secondary/50 mb-2 text-xs w-fit font-semibold z-15 relative
+					bg-transparent mix-blend-difference rounded-none border-secondary/50 border-[1.5px] px-1 shadow-md"
+        >
           {mainParentFolder?.update_date}
         </h2>
-        <h3 class="text-secondary/50 mb-2 text-xs w-fit font-semibold z-15 relative 
-					bg-transparent mix-blend-difference rounded-none border-secondary/50 border-[1.5px] px-1 shadow-md">
+        <h3
+          class="text-secondary/50 mb-2 text-xs w-fit font-semibold z-15 relative
+					bg-transparent mix-blend-difference rounded-none border-secondary/50 border-[1.5px] px-1 shadow-md"
+        >
           {mainParentFolder?.update_time}
         </h3>
       </div>
-      <div class="relative w-fit flex items-start gap-2"> {/* Flex container for the image and SVG */}
+      <div class="relative w-fit flex items-start gap-2">
+        {" "}
+        {/* Flex container for the image and SVG */}
         <Show when={mainParentFolder.title && mainParentFolder.last_watched_video}>
           <img
             alt={mainParentFolder?.title}
             src={FILE_SRC_LWV_COVER_IMG_PATH}
-            class="select-none h-[175px] md:h-[200px] object-contain lg:h-[240px] 
-              w-auto z-30 rounded-none 
-              border-transparent border-2 shadow-md"
+            class="select-none object-contain
+              h-[220px] md:h-[270px] lg:h-[290px] xl:h-[315px]
+              w-auto z-30 rounded-sm
+              border-white/10 border-2 shadow-md"
           />
           <svg
-            class="text-secondary fill-secondary bg-primary/50 rounded-sm hover:opacity-50 transition-all
-              cursor-pointer h-auto w-1/4 p-1 mix-blend-multiply absolute z-50 shadow-md shadow-primary/20"
+            class="text-secondary fill-secondary/80 bg-primary/70 rounded-sm hover:opacity-50 transition-all
+              cursor-pointer h-auto w-1/4 p-1 mix-blend-hard-light absolute z-50 shadow-md shadow-primary/20"
             style={{
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)' // Center the Play button within the image
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)", // Center the Play button within the image
             }}
             xmlns="http://www.w3.org/2000/svg"
             width="30"
@@ -69,4 +76,3 @@ export default function ({ mainParentFolder, user }: { mainParentFolder: OsFolde
     </header>
   );
 }
-
