20e4924925fd14b29819e690c3f36a85e4aaf7aa
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-23 20:26:31 -0800

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -8,22 +8,27 @@ import get_default_user from "../tauri-cmds/users";
 import mpv_system_check from "../tauri-cmds/mpv/mpv_system_check";
 import ErrorAlert from "../main-components/error-alert";
 import { useNavigate } from "@solidjs/router";
-import { Skeleton } from "../components/ui/skeleton";
 
 export default function Dashboard() {
   const [user, setUser] = createSignal<UserType | null>(null);
   const [osFolders, { refetch }] = createResource<OsFolder[]>(async () => {
     const user = await get_default_user();
     if (user) {
-      setUser(user);
       const folders = await get_os_folders(user.id);
-      return folders || []; 
+      setUser(user);
+      return folders || [];
     }
-    return []; 
+    return [];
   });
 
-  const [error] = createResource(() => mpv_system_check(user()?.settings.mpv_path));
-  const navigate = useNavigate();
+  const [error] = createResource(async () => {
+    const path = await get_default_user().then(user => user?.settings.mpv_path || null);
+    if (path) {
+      return await mpv_system_check(path);
+    }
+    return null;
+  }); 
+	const navigate = useNavigate();
 
   return (
     <main>
@@ -32,14 +37,9 @@ export default function Dashboard() {
         <ErrorAlert error={error()!} onClick={() => navigate(`/settings/mpv_ERROR_${error()}`)} />
       </Show>
       <section class="h-fit py-4 px-3 md:px-16 lg:px-36 xl:px-44 flex flex-row gap-2">
-        <div class="grid grid-cols-4 gap-2 min-h-[calc(4*cardHeight)]">
+        <div class="grid grid-cols-4 gap-2">
           <Show
             when={user() && osFolders.state === "ready" && osFolders()!.length > 0}
-            fallback={
-              osFolders.state === "pending" && (
-                <Skeleton class="h-32 w-24 sm:h-44 sm:w-32 md:h-48 md:w-36 lg:h-52 lg:w-40" />
-              )
-            }
           >
             <For each={osFolders()}>
               {(folder) => (
@@ -47,9 +47,7 @@ export default function Dashboard() {
               )}
             </For>
           </Show>
-          <Show when={user()}>
-            <AddNewSkeleton user={user} refetch={refetch} />
-          </Show>
+          <AddNewSkeleton user={user} refetch={refetch} />
         </div>
       </section>
     </main>
