de21eca6b53b22f4358c3f3c862af4adc18120d1
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-23 20:26:40 -0800

    Update folders-section.tsx

--- src/routes/folder-library/folders-section.tsx
+++ src/routes/folder-library/folders-section.tsx
@@ -1,45 +1,36 @@
-import { Accessor, createSignal, For, Show } from "solid-js";
-import { OsFolder, OsVideo, UserType } from "../../models";
-import play_video from "../../tauri-cmds/mpv/play_video";
-import ErrorAlert from "../../main-components/error-alert";
+import { For, Resource } from "solid-js";
+import { OsFolder, UserType } from "../../models";
 import LibraryFolderCard from "./folder-card";
-import { A, useLocation, useNavigate } from "@solidjs/router";
+import { useNavigate } from "@solidjs/router";
 
 export default function LibraryFoldersSection({
   user,
   mainParentFolder,
-  childFolders
+  childFolders,
 }: {
-  user: UserType;
-  mainParentFolder: OsFolder;
-  childFolders: OsFolder[];
+  user: Resource<UserType | null>;
+  mainParentFolder: Resource<OsFolder | null>;
+  childFolders: Resource<OsFolder[] | null>;
 }
 ) {
   const navigate = useNavigate();
-  const [error, setError] = createSignal<string | null>();
 
   return (
     <>
-      <Show when={error()}>
-        <ErrorAlert error={error()!} />
-      </Show>
       <section
         class="md:px-4 overflow-hidden w-full h-fit px-2 pb-4 relative 
 				border-b-white border-b-2 shadow-lg shadow-primary/10">
         <ul
           class="mx-auto h-fit w-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
 					xl:grid-cols-4 gap-6 sm:gap-10 justify-center items-center">
-          <For each={childFolders}>
+          <For each={childFolders()}>
             {(folder, index) => (
               <LibraryFolderCard
                 index={index}
                 folder={folder}
                 mainParentFolder={mainParentFolder}
                 onClick={() => {
-                  navigate(`/library/${encodeURIComponent(folder.path)}`)
-                  setTimeout(() => {
-                  window.location.reload();
-                  }, 1)
+                  navigate(`/library/${encodeURIComponent(folder.path)}`);
                 }}
               />
             )}
