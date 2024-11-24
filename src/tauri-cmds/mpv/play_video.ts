d0f74fe93d6e7e656cc8b52953eadc51719bfd5d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-23 20:26:49 -0800

    Update play_video.ts

--- src/tauri-cmds/mpv/play_video.ts
+++ src/tauri-cmds/mpv/play_video.ts
@@ -1,10 +1,10 @@
 import { invoke } from "@tauri-apps/api/core";
 import { OsFolder, OsVideo, UserType } from "../../models";
 
-export default async function play_video(mainFolder: OsFolder, video: OsVideo, user: UserType) {
+export default async function play_video(mainFolder: OsFolder, osVideos: OsVideo[], video: OsVideo, user: UserType) {
   try {
-		console.log("playing video")
-    await invoke("play_video", { mainFolder, video, user });
+    console.log("playing video")
+    await invoke("play_video", { mainFolder, osVideos, video, user });
     return null;
   } catch (e) {
     return e as string;
