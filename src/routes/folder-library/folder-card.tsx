8842e51023fbc04f5317a3547713421cdf481344
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-23 20:26:39 -0800

    Update folder-card.tsx

--- src/routes/folder-library/folder-card.tsx
+++ src/routes/folder-library/folder-card.tsx
@@ -1,5 +1,5 @@
 import { OsFolder, } from "../../models";
-import { Accessor, createEffect, Show } from "solid-js";
+import { Accessor, createEffect, Resource, Show } from "solid-js";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import {
   ContextMenu,
@@ -7,6 +7,7 @@ import {
 } from "../../components/ui/context-menu";
 import { Transition } from "solid-transition-group";
 import { IconFolderFilled } from "@tabler/icons-solidjs";
+import { FolderDescription } from "../../main-components/description/folder-desc";
 
 const LibraryFolderCard = ({
   index,
@@ -16,7 +17,7 @@ const LibraryFolderCard = ({
 }: {
   index: Accessor<number>;
   folder: OsFolder;
-  mainParentFolder: OsFolder;
+  mainParentFolder: Resource<OsFolder | null>;
   onClick: (event: MouseEvent) => void;
 }) => {
 
@@ -34,13 +35,12 @@ const LibraryFolderCard = ({
     >
       <ContextMenu>
         <ContextMenuTrigger>
-          <div class="h-auto max-h-[120px] max-w-[450px] min-h-30 cursor-pointer relative w-full border-[1.5px] 
+          <div class="h-56 max-w-[450px] cursor-pointer relative w-full border-[1.5px] 
 						border-transparent rounded-none shadow-black/30 shadow-md flex items-center 
 						justify-center overflow-hidden will-change-transform transition-all group"
             onClick={onClick}
           >
 
-            <div class="w-7 h-7 bg-secondary absolute left-0 top-0 z-20 rounded-br-sm" />
             {/* Folder Image */}
             <div class="folder-card-container inset-0"
             >
@@ -53,26 +53,18 @@ const LibraryFolderCard = ({
             </div>
 
             {/* Hover Overlay for Extended Description */}
-            <div class="absolute inset-0 bg-black/80 opacity-0 group-hover:opacity-100 transition-opacity duration-200
-                        flex items-center justify-center text-white p-4 z-20">
-              <p class="text-sm font-medium absolute left-2 top-2 text text-zinc-100 bg-transparent 
-												 mix-blend-difference w-fit z-10 shadow-2xl rounded-none px-0.5">
-                {folder.title}
-              </p>
-              <p class="text-[12px] font-medium absolute left-2 bottom-2 text text-zinc-300 bg-transparent 
-												mix-blend-difference w-fit z-10 shadow-2xl rounded-none px-0.5">
-                {folder.update_date}
-              </p>
-              <p class="text-[13px] font-medium absolute left-2 bottom-6 text text-zinc-300 bg-transparent 
-												mix-blend-difference w-fit z-10 shadow-2xl rounded-none px-0.5">
-                {folder.update_time}
-              </p>
-            </div>
+            <FolderDescription
+              folder={() => folder}
+            />
 
             {/* folder.Title at Bottom */}
-            <div class="h-fit absolute left-0 bottom-0 bg-primary/80 
-												border-t-2 border-t-secondary/10 shadow-md 
-                        text-border text-xs p-1 mix-blend-plus-darker group-hover:opacity-0 transition-opacity duration-300">
+            <div class="h-fit font-semibold absolute left-0 bottom-0 bg-primary/80 
+												border-t-4 border-t-secondary/10 shadow-md 
+                        text-border text-xs p-1 mix-blend-plus-darker 
+							rounded-tr-sm
+							backdrop-blur-md
+							group-hover:opacity-0 transition-opacity duration-200">
+              {folder.title}
             </div>
 
           </div>
