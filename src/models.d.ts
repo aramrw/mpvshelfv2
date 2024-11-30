b2f47b162ae51a8dc10bbd89da6b2d911c91ce90
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-29 16:54:23 -0800

    Update models.d.ts

--- src/models.d.ts
+++ src/models.d.ts
@@ -35,9 +35,27 @@ export type OsVideo = {
   path: string;
   title: string;
   cover_img_path: string | undefined;
+	metadata: FileMetadata;
   watched: bool;
   duration: number;
   position: number;
   update_date: string;
   update_time: string;
+}
+
+export type FileMetadata = {
+  created: number;
+  modified: number;
+  accessed: number;
+  size: number;
+}
+
+export type FolderMetadata = {
+  contains: FolderContains;
+	size: number;
+}
+
+export type FolderContains = {
+  files: number;
+  folders: number;
 }
