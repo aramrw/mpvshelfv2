a35485af57f0057812523ee2b939ce062e96c4de
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-14 22:45:58 -0800

    Update add-new-skeleton.tsx

--- src/dashboard/components/add-new-skeleton.tsx
+++ src/dashboard/components/add-new-skeleton.tsx
@@ -4,7 +4,7 @@ import { open } from '@tauri-apps/plugin-dialog';
 import { OsFolder, UserType } from "../../models";
 import { Accessor, createSignal, Show } from "solid-js";
 import { Transition } from "solid-transition-group";
-import upsert_read_os_dir from "../../tauri-cmds/upsert_read_os_dir";
+import upsert_read_os_dir from "../../tauri-cmds/os_folders/upsert_read_os_dir";
 
 const AddNewSkeleton = ({
   user,
@@ -35,59 +35,39 @@ const AddNewSkeleton = ({
     >
 
       <Popover placement="right-start">
-        <PopoverTrigger class="h-fit">
-          <div class="w-fit relative flex items-center justify-center hover:opacity-50 transition-opacity cursor-pointer duration-100 ease-in-out">
+        <PopoverTrigger class="h-fit w-fit">
+          <div class="relative flex w-fit cursor-pointer items-center justify-center transition-opacity duration-100 ease-in-out hover:opacity-50">
             <div
-              class="
-							h-32 w-24 
-							sm:h-44 sm:w-32 
-							md:h-48 md:w-36 
-							lg:h-64 lg:w-48
-							xl:h-80 xl:w-56 
-							rounded-sm shadow-md bg-white" />
+              class="h-32 w-24 rounded-sm bg-white shadow-md sm:h-44 sm:w-32 md:h-48 md:w-36 lg:h-64 lg:w-48 xl:h-80 xl:w-56" />
             <Show when={!isCreating()}
               fallback={
-                <IconLoader2 class="
-							w-9 h-9 
-							sm:h-11 sm:w-11 
-							md:h-14 md:w-14 
-							lg:h-20 lg:w-20 
-							xl:h-24 xl:w-24 
-							text-zinc-300 stroke-[1.8] animate-spin
-							absolute top-0 bottom-0 left-0 right-0 m-auto"
+                <IconLoader2 class="absolute bottom-0 left-0 right-0 top-0 m-auto h-9 w-9 animate-spin stroke-[1.8] text-zinc-300 sm:h-11 sm:w-11 md:h-14 md:w-14 lg:h-20 lg:w-20 xl:h-24 xl:w-24"
                 />
               }
             >
-              <IconPlus class="
-							w-9 h-9 
-							sm:h-11 sm:w-11 
-							md:h-14 md:w-14 
-							lg:h-20 lg:w-20 
-							xl:h-24 xl:w-24 
-							text-zinc-300 stroke-[1.8] 
-							absolute top-0 bottom-0 left-0 right-0 m-auto"
+              <IconPlus class="absolute bottom-0 left-0 right-0 top-0 m-auto h-9 w-9 stroke-[1.8] text-zinc-300 sm:h-11 sm:w-11 md:h-14 md:w-14 lg:h-20 lg:w-20 xl:h-24 xl:w-24"
               />
             </Show>
           </div>
         </PopoverTrigger>
-        <PopoverContent class="p-0 bg-transparent border-none shadow-none text-sm flex flex-col justify-center items-center font-medium">
-          <ul class="w-fit h-full flex flex-col gap-1">
-            <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
+        <PopoverContent class="flex flex-col items-center justify-center border-none bg-transparent p-0 text-sm font-medium shadow-none">
+          <ul class="flex h-full w-fit flex-col gap-1">
+            <li class="flex w-fit cursor-pointer flex-row items-center justify-center gap-0.5 rounded-sm bg-white p-1 shadow-md transition-opacity duration-100 ease-in-out hover:opacity-50"
               onClick={async () => {
                 setIsCreating(true);
                 let dir_path = await open({ directory: true });
                 if (dir_path) {
-                  await upsert_read_os_dir(dir_path, undefined, user()!.id, undefined, undefined);
+                  await upsert_read_os_dir(dir_path, undefined, user()!, undefined, undefined);
                   refetch();
                 }
                 setIsCreating(false);
               }}
             >
-              <IconFolderPlus class="w-6 h-6 sm:h-8 sm:w-8 md:h-9 md:w-9 lg:w-10 lg:h-10 text-primary fill-zinc-500 stroke-[2.2]" />
+              <IconFolderPlus class="h-6 w-6 fill-zinc-500 stroke-[2.2] text-primary sm:h-8 sm:w-8 md:h-9 md:w-9 lg:h-10 lg:w-10" />
             </li>
-            <li class="bg-white p-1 rounded-sm shadow-md flex flex-row justify-center items-center w-fit gap-0.5 hover:opacity-50 transition-opacity duration-100 ease-in-out cursor-pointer"
+            <li class="flex w-fit cursor-pointer flex-row items-center justify-center gap-0.5 rounded-sm bg-white p-1 shadow-md transition-opacity duration-100 ease-in-out hover:opacity-50"
             >
-              <IconFilePlus class="w-6 h-6 sm:h-8 sm:w-8 md:h-9 md:w-9 lg:h-10 lg:w-10 text-primary fill-zinc-500 stroke-[2.2]" />
+              <IconFilePlus class="h-6 w-6 fill-zinc-500 stroke-[2.2] text-primary sm:h-8 sm:w-8 md:h-9 md:w-9 lg:h-10 lg:w-10" />
             </li>
           </ul>
         </PopoverContent>
