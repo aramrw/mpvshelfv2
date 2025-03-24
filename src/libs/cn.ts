b3ddcd30aa692c47f243d5890f3855886f2a7419
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-24 13:21:49 -0700

    Update cn.ts

--- src/libs/cn.ts
+++ src/libs/cn.ts
@@ -3,3 +3,7 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...classLists: ClassValue[]) => twMerge(clsx(classLists));
+
+export function escapeCSSUrl(url: string) {
+  return url.replace(/\(/g, '\\(').replace(/\)/g, '\\)');
+}
